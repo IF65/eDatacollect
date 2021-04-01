4ba81d65a19d5dc1efeb5ed7429d6df279b39a2c
Marco Gnecchi <marco.gnecchi@if65.it>
2021-04-01 13:43:15 +0200

    aggiunto recordcode1 a V e T

--- src/eDatacollect.php
+++ src/eDatacollect.php
@@ -14,9 +14,9 @@ $timeZone = new \DateTimeZone('Europe/Rome');
 if ($debug) {
     //$input = "{\"function\":\"recuperaMTXRigheFatture\",\"ddate\":\"2021-01-03\",\"store\":\"0101\",\"reg\":\"001\",\"trans\":\"6228\"}";
     //$input = "{\"function\":\"creazioneDatacollectTcPos\",\"data\":\"2021-01-30\",\"sede\":\"0501\"}";
-    //$input = "{\"function\":\"recuperaDatiPerQuadratura\",\"data\":\"2021-01-31\",\"sede\":\"0138\"}";
+    //$input = "{\"function\":\"recuperaDatiPerQuadratura\",\"data\":\"2021-03-21\",\"sede\":\"3152\"}";
     //$input = "{\"function\":\"creazioneDatacollect\",\"data\":\"2021-02-17\",\"sede\":\"0155\",\"cassa\":\"052\",\"transazione\":\"0309\"}";
-	$input = "{\"function\":\"creazioneDatacollect\",\"data\":\"2021-03-16\",\"sede\":\"3689\"}";
+	$input = "{\"function\":\"creazioneDatacollect\",\"data\":\"2021-03-30\",\"sede\":\"0114\"}";
     //$input = "{\"function\":\"recuperaFatture\"}";
     //$input = "{\"function\":\"creaFileInterscambioFatture\"}";
     //$input = "{\"function\":\"creazioneDatacollectRiepvegiTxt\",\"data\":\"2020-09-06\",\"sede\":\"0501\"}";
@@ -24,7 +24,7 @@ if ($debug) {
     //file_put_contents('/Users/if65/Desktop/testDC.txt', implode("\n",json_decode($result, true)));
     //file_put_contents('/Users/if65/Desktop/testFatture.txt', $result);
     //$input = "{\"function\":\"recuperaBarcode\",\"articoli\":[\"6364070\"]}";
-    //$input = "{\"function\":\"recuperaSospesi\",\"data\":\"2020-11-25\",\"sede\":\"0101\"}";
+    //$input = "{\"function\":\"recuperaSospesi\",\"data\":\"2021-03-24\",\"sede\":\"0201\"}";
 	//$input = "{\"function\":\"creaFatturaMtx\",\"ddate\":\"2021-01-13\",\"store\":\"0128\",\"reg\":\"002\",\"trans\":\"5041\"}";
 
 	//$input = "{\"function\":\"incassiInTempoReale\",\"data\":\"2021-02-11\"}";
