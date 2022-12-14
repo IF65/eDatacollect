91e8232c532a9a3e6b7f68842584ca3ada7d29b0
Marco Gnecchi <marco.gnecchi@if65.it>
2022-12-14 14:44:14 +0100

    aggiunto cisano e gorle tcpos

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -13,245 +13,240 @@ use PDOException;
 class Database
 {
 
-	protected $pdo = null;
-	private $db = [];
+    protected $pdo = null;
+    private $db = [];
 
-	public $t_idc = null;
-	public $v_tcp_transazioni = null;
-	public $v_mtx_fatture = null;
+    public $t_idc = null;
+    public $v_tcp_transazioni = null;
+    public $v_mtx_fatture = null;
 
-	private $sqlDetails = null;
+    private $sqlDetails = null;
 
-	public function __construct(array $sqlDetails, $loadDb = True)
-	{
-		$this->sqlDetails = $sqlDetails;
-		$this->loadDb = $loadDb;
-		$conStr = sprintf("mysql:host=%s", $sqlDetails['host']);
-		try {
-			$this->pdo = new PDO($conStr, $sqlDetails['user'], $sqlDetails['password'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-			//$this->pdoMsSrv = new PDO("sqlsrv:Server=10.11.14.250,9089;Database=TCPOS4", "sa", "vfr456YHN");
-			$this->db = $sqlDetails['db'];
+    public function __construct(array $sqlDetails, $loadDb = true)
+    {
+        $this->sqlDetails = $sqlDetails;
+        $this->loadDb = $loadDb;
+        $conStr = sprintf("mysql:host=%s", $sqlDetails['host']);
+        try {
+            $this->pdo = new PDO(
+                $conStr,
+                $sqlDetails['user'],
+                $sqlDetails['password'],
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+            );
+            //$this->pdoMsSrv = new PDO("sqlsrv:Server=10.11.14.250,9089;Database=TCPOS4", "sa", "vfr456YHN");
+            $this->db = $sqlDetails['db'];
 
-			self::createDatabase();
+            self::createDatabase();
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
 
-		} catch (PDOException $e) {
-			die($e->getMessage());
-		}
-	}
+    public function createDatabase()
+    {
+        try {
+            // creazione schemi
+            // ----------------------------------------------------------
+            foreach ($this->db as $key => $value) {
+                $stmt = $this->pdo->prepare("create database if not exists $value;");
+                $stmt->execute();
+            }
 
-	public function createDatabase()
-	{
-		try {
-
-			// creazione schemi
-			// ----------------------------------------------------------
-			foreach ($this->db as $key => $value) {
-				$stmt = $this->pdo->prepare("create database if not exists $value;");
-				$stmt->execute();
-			}
-
-			// creazione tabelle
-			// ----------------------------------------------------------
-			$this->t_idc = new TIdc($this->pdo, $this->db['mtx'], 'idc');
+            // creazione tabelle
+            // ----------------------------------------------------------
+            $this->t_idc = new TIdc($this->pdo, $this->db['mtx'], 'idc');
 
 
-			// apertura viste
-			// ----------------------------------------------------------
-			$this->v_tcp_transazioni = new VTcp_transazioni();
-			$this->v_mtx_fatture = new VMtx_fatture($this->pdo);
+            // apertura viste
+            // ----------------------------------------------------------
+            $this->v_tcp_transazioni = new VTcp_transazioni();
+            $this->v_mtx_fatture = new VMtx_fatture($this->pdo);
 
-			return true;
-		} catch (PDOException $e) {
-			die("DB ERROR: " . $e->getMessage());
-		}
-	}
+            return true;
+        } catch (PDOException $e) {
+            die("DB ERROR: " . $e->getMessage());
+        }
+    }
 
-	public function recuperaDatiContabili(array $request): string
-	{
-		return $this->t_idc->recuperaDatiContabili($request);
-	}
+    public function recuperaDatiContabili(array $request): string
+    {
+        return $this->t_idc->recuperaDatiContabili($request);
+    }
 
-	public function recuperaDatiPerQuadratura(array $request): string
-	{
-		if ($request['sede'] == '0501' || $request['sede'] == '0201' || $request['sede'] == '0155' || $request['sede'] == '0142' || $request['sede'] == '0203' || $request['sede'] == '0204' || $request['sede'] == '0132' || $request['sede'] == '0148' || $request['sede'] == '0115' || $request['sede'] == '0101' || $request['sede'] == '0205'|| $request['sede'] == '0104'|| $request['sede'] == '0133'|| $request['sede'] == '3661') {
-			$casseTCPOS = json_decode($this->v_tcp_transazioni->recuperaDatiPerQuadratura($request), true);
-			$casseASAR = json_decode($this->t_idc->recuperaDatiPerQuadratura($request), true);
+    public function recuperaDatiPerQuadratura(array $request): string
+    {
+        if ($request['sede'] == '0501' || $request['sede'] == '0201' || $request['sede'] == '0155' || $request['sede'] == '0142' || $request['sede'] == '0203' || $request['sede'] == '0204' || $request['sede'] == '0132' || $request['sede'] == '0148' || $request['sede'] == '0115' || $request['sede'] == '0101' || $request['sede'] == '0205' || $request['sede'] == '0104' || $request['sede'] == '0133' || $request['sede'] == '3661' || $request['sede'] == '0125' || $request['sede'] == '0139') {
+            $casseTCPOS = json_decode($this->v_tcp_transazioni->recuperaDatiPerQuadratura($request), true);
+            $casseASAR = json_decode($this->t_idc->recuperaDatiPerQuadratura($request), true);
 
-			$result = [];
-			foreach ($casseTCPOS as $riga) {
-				if (preg_match('/^(0500)(.*)$/', $riga['id'], $matches)) {
-					$riga['id'] = '0501' . $matches[2];
-				}
-				if (preg_match('/^(\d{4}).*$/', $riga['id'], $matches)) {
-					if ($matches[1] == $request['sede']) {
-						$result[] = $riga;
-					}
-				}
-			}
+            $result = [];
+            foreach ($casseTCPOS as $riga) {
+                if (preg_match('/^(0500)(.*)$/', $riga['id'], $matches)) {
+                    $riga['id'] = '0501' . $matches[2];
+                }
+                if (preg_match('/^(\d{4}).*$/', $riga['id'], $matches)) {
+                    if ($matches[1] == $request['sede']) {
+                        $result[] = $riga;
+                    }
+                }
+            }
 
-			foreach ($casseASAR as $riga) {
-				$result[] = $riga;
-			}
+            foreach ($casseASAR as $riga) {
+                $result[] = $riga;
+            }
 
-			return json_encode($result, true);
+            return json_encode($result, true);
+        } elseif ($request['sede'] == '0502' || $request['sede'] == '0503') {
+            return $this->v_tcp_transazioni->recuperaDatiPerQuadratura($request);
+        } else {
+            return $this->t_idc->recuperaDatiPerQuadratura($request);
+        }
+    }
 
-		} elseif ($request['sede'] == '0502' || $request['sede'] == '0503') {
-			return $this->v_tcp_transazioni->recuperaDatiPerQuadratura($request);
+    public function creazioneDatacollectTcPos(array $request): string
+    {
+        return $this->v_tcp_transazioni->creazioneDatacollectTcPos($request);
+    }
 
-		} else {
-			return $this->t_idc->recuperaDatiPerQuadratura($request);
+    public function incassoPeriodo(array $request): string
+    {
+        return $this->t_idc->incassoPeriodo($request);
+    }
 
-		}
+    public function creazioneDatacollect(array $request): string
+    {
+        return $this->t_idc->creazioneDatacollect($request);
+    }
 
-	}
+    public function creazioneDatacollectEpipoli(array $request): string
+    {
+        return $this->v_tcp_transazioni->creazioneDatacollectEpipoli($request);
+    }
 
-	public function creazioneDatacollectTcPos(array $request): string
-	{
-		return $this->v_tcp_transazioni->creazioneDatacollectTcPos($request);
+    public function creazioneDatacollectRiepvegi(array $request): string
+    {
+        $datacollectRiepvegi = json_decode($this->v_tcp_transazioni->creazioneDatacollectRiepvegi($request), true);
 
-	}
+        $articoli = [];
+        foreach ($datacollectRiepvegi as $codice => $row) {
+            $articoli[] = $codice;
+        }
 
-	public function incassoPeriodo(array $request): string
-	{
-		return $this->t_idc->incassoPeriodo($request);
-	}
+        $articoliBarcode = $this->t_idc->recuperaBarcode($articoli);
+        foreach ($articoliBarcode as $codice => $barcode) {
+            $datacollectRiepvegi[$codice]['barcode'] = $barcode;
+        }
 
-	public function creazioneDatacollect(array $request): string
-	{
-		return $this->t_idc->creazioneDatacollect($request);
-	}
+        return json_encode($datacollectRiepvegi);
+    }
 
-	public function creazioneDatacollectEpipoli(array $request): string
-	{
-		return $this->v_tcp_transazioni->creazioneDatacollectEpipoli($request);
-	}
+    public function recuperaBarcode($request): string
+    {
+        return json_encode($this->t_idc->recuperaBarcode($request));
+    }
 
-	public function creazioneDatacollectRiepvegi(array $request): string
-	{
-		$datacollectRiepvegi = json_decode($this->v_tcp_transazioni->creazioneDatacollectRiepvegi($request), true);
+    public function recuperaReparto($request): string
+    {
+        return json_encode($this->t_idc->recuperaReparto($request));
+    }
 
-		$articoli = [];
-		foreach ($datacollectRiepvegi as $codice => $row) {
-			$articoli[] = $codice;
-		}
+    public function recuperaCodiceArticoliPeso(): string
+    {
+        return json_encode($this->t_idc->recuperaCodiceArticoliPeso());
+    }
 
-		$articoliBarcode = $this->t_idc->recuperaBarcode($articoli);
-		foreach ($articoliBarcode as $codice => $barcode) {
-			$datacollectRiepvegi[$codice]['barcode'] = $barcode;
-		}
+    public function creazioneDatacollectRiepvegiTxt(array $request): string
+    {
+        $dati = $this->creazioneDatacollectRiepvegi($request);
+        return $this->v_tcp_transazioni->creazioneDatacollectRiepvegiTxt($request, $dati);
+    }
 
-		return json_encode($datacollectRiepvegi);
-	}
+    public function incassiInTempoReale(array $request): string
+    {
+        /*$rows = [];
 
-	public function recuperaBarcode($request): string
-	{
-		return json_encode($this->t_idc->recuperaBarcode($request));
+        $tcpos = $this->v_tcp_transazioni->incassiInTempoReale($request);
+        foreach($tcpos as $row) {
+            $index = $row['store'] . $row['ddate'];
+            $rows[$index] = [
+                'ddate' => $row['ddate'],
+                'store' => $row['store'],
+                'totalamount' => $row['totalamount'] * 1,
+                'customerCount' => $row['customerCount'] * 1
+            ];
+        }
 
-	}
+        $asar = $this->t_idc->incassiInTempoReale($request);
+        foreach($asar as $row) {
+            $index = $row['store'] . $row['ddate'];
 
-	public function recuperaReparto($request): string
-	{
-		return json_encode($this->t_idc->recuperaReparto($request));
+            if (key_exists($index, $rows)) {
+                $rows[$index]['totalamount'] += $row['totalamount'];
+                $rows[$index]['customerCount'] += $row['customerCount'];
+            } else {
+                $rows[$index] = [
+                    'ddate' => $row['ddate'],
+                    'store' => $row['store'],
+                    'totalamount' => $row['totalamount'] * 1,
+                    'customerCount' => $row['customerCount'] * 1
+                ];
+            }
+        }*/
 
-	}
+        $rows = $this->t_idc->incassiInTempoReale($request);
 
-	public function recuperaCodiceArticoliPeso(): string
-	{
-		return json_encode($this->t_idc->recuperaCodiceArticoliPeso());
+        return json_encode($rows);
+    }
 
-	}
+    public function recuperaMTXRigheFatture(array $request): string
+    {
+        return $this->t_idc->recuperaMTXRigheFatture($request);
+    }
 
-	public function creazioneDatacollectRiepvegiTxt(array $request): string
-	{
-		$dati = $this->creazioneDatacollectRiepvegi($request);
-		return $this->v_tcp_transazioni->creazioneDatacollectRiepvegiTxt($request, $dati);
-	}
+    public function dettaglioQuadratura(array $request): string
+    {
+        return $this->t_idc->dettaglioQuadratura($request);
+    }
 
-	public function incassiInTempoReale(array $request): string
-	{
-		/*$rows = [];
+    public function aggiornaStatoQuadratura(array $request): array
+    {
+        return $this->t_idc->aggiornaStatoQuadratura($request);
+    }
 
-		$tcpos = $this->v_tcp_transazioni->incassiInTempoReale($request);
-		foreach($tcpos as $row) {
-			$index = $row['store'] . $row['ddate'];
-			$rows[$index] = [
-				'ddate' => $row['ddate'],
-				'store' => $row['store'],
-				'totalamount' => $row['totalamount'] * 1,
-				'customerCount' => $row['customerCount'] * 1
-			];
-		}
+    public function recuperaFatture(): string
+    {
+        return $this->v_tcp_transazioni->recuperaFatture();
+    }
 
-		$asar = $this->t_idc->incassiInTempoReale($request);
-		foreach($asar as $row) {
-			$index = $row['store'] . $row['ddate'];
+    public function creaFileInterscambioFatture(string $fatture): string
+    {
+        return $this->v_tcp_transazioni->creaFileInterscambioFatture($fatture);
+    }
 
-			if (key_exists($index, $rows)) {
-				$rows[$index]['totalamount'] += $row['totalamount'];
-				$rows[$index]['customerCount'] += $row['customerCount'];
-			} else {
-				$rows[$index] = [
-					'ddate' => $row['ddate'],
-					'store' => $row['store'],
-					'totalamount' => $row['totalamount'] * 1,
-					'customerCount' => $row['customerCount'] * 1
-				];
-			}
-		}*/
+    public function elencoFattureEmesse(): string
+    {
+        return $this->v_tcp_transazioni->elencoFattureEmesse();
+    }
 
-		$rows = $this->t_idc->incassiInTempoReale($request);
+    public function creaFatturaMtx(array $request): string
+    {
+        return $this->v_mtx_fatture->creaFattura($request);
+    }
 
-		return json_encode($rows);
-	}
+    public function elencoTransazioni(array $request): array
+    {
+        return $this->t_idc->elencoTransazioni($request);
+    }
 
-	public function recuperaMTXRigheFatture(array $request): string
-	{
-		return $this->t_idc->recuperaMTXRigheFatture($request);
-	}
+    public function creaJsonFattura(array $request): string
+    {
+        return $this->t_idc->creaJsonFattura($request);
+    }
 
-	public function dettaglioQuadratura(array $request): string
-	{
-		return $this->t_idc->dettaglioQuadratura($request);
-	}
-
-	public function aggiornaStatoQuadratura(array $request): array
-	{
-		return $this->t_idc->aggiornaStatoQuadratura($request);
-	}
-
-	public function recuperaFatture(): string
-	{
-		return $this->v_tcp_transazioni->recuperaFatture();
-	}
-
-	public function creaFileInterscambioFatture(string $fatture): string
-	{
-		return $this->v_tcp_transazioni->creaFileInterscambioFatture($fatture);
-	}
-
-	public function elencoFattureEmesse(): string
-	{
-		return $this->v_tcp_transazioni->elencoFattureEmesse();
-	}
-
-	public function creaFatturaMtx(array $request): string
-	{
-		return $this->v_mtx_fatture->creaFattura($request);
-	}
-
-	public function elencoTransazioni(array $request): array
-	{
-		return $this->t_idc->elencoTransazioni($request);
-	}
-
-	public function creaJsonFattura(array $request): string
-	{
-		return $this->t_idc->creaJsonFattura($request);
-	}
-
-	public function __destruct()
-	{
-		$this->pdo = null;
-	}
+    public function __destruct()
+    {
+        $this->pdo = null;
+    }
 }
 
