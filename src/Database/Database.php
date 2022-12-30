4ae61703d9d3990666f4f6fc10f3633d9a0934b4
Marco Gnecchi <marco.gnecchi@if65.it>
2022-12-30 09:05:38 +0100

    aggiunti a tcpos i negozi  0140, 0141 e 0202

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -85,7 +85,7 @@ class Database
             $request['sede'] == '0190' || $request['sede'] == '0131' || $request['sede'] == '0129' || $request['sede'] == '3152' ||
             $request['sede'] == '0127' || $request['sede'] == '0119' || $request['sede'] == '0143' || $request['sede'] == '0124' ||
             $request['sede'] == '0141' || $request['sede'] == '0114' || $request['sede'] == '0177' || $request['sede'] == '3682' ||
-            $request['sede'] == '0128'
+            $request['sede'] == '0128' || $request['sede'] == '0202' || $request['sede'] == '0140' || $request['sede'] == '0141'
         ) {
             $casseTCPOS = json_decode($this->v_tcp_transazioni->recuperaDatiPerQuadratura($request), true);
             $casseASAR = json_decode($this->t_idc->recuperaDatiPerQuadratura($request), true);
